fb6739c1a6152dda671c670bd6dd73d7fa7fae0d
amaan123056 <afowaz@yahoo.com>
2024-11-25 20:50:33 +0400

    commit a

---
+++ Imathrowadoritoatyourface.py
@@ -0,0 +1,6 @@
+string=input("Enter the string you want to reverse : ")
+string2=("")
+for i in string:
+    string2=i+string2
+print("The og string is string:",string)
+print("The reversed string is:",string2)
